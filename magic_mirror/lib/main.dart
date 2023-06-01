a0ef9d3d2fa237cf81f67743dba2f179ad546076
Matthew - PC <tansenmatt@gmail.com>
2023-06-01 14:36:29 +1200

    'bla'

--- magic_mirror/lib/main.dart
+++ magic_mirror/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(ss
+    return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Namer App',
